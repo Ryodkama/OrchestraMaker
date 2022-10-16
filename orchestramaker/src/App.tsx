056a321a208c12e2094e885430249588913d72db
Ryosuke <ryosuke.kamada.1998@gmail.com>
2022-10-16 19:11:13 +0900

    react routerの追加

--- orchestramaker/src/App.tsx
+++ orchestramaker/src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<FrontPage />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+);
+
+const Header = () => (
+  <>
+    <title>オーケストラメイカー</title>
+    <h1>ログインページ</h1>
+
+    <div>
+      新規登録は<Link to={`/signup`}>こちら</Link>
+    </div>
+    <div>
+      <Link to={`/`}>ホームに戻る</Link>
+    </div>
+  </>
+);
+
+const Footer = () => {
+  const style = {
+    width: "100px",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul style={style}>
+        <li>
+          <Link to="/">トップページ</Link>
+        </li>
+        <li>
+          <Link to="/about">私たちについて</Link>
+        </li>
+      </ul>
     </div>
   );
-}
+};
+
+const FrontPage = () => {
+  return (
+    <div>
+      私たちのAIはオーケストレーションをサポートすることによって,編曲を行う際の,初心者のハードルを下げます.
+      •ピアノなどの楽譜データからバンド構成または,フルオーケストラ構成へオーケストレーションを自動で行い,編曲をするAIの制作を目指す
+    </div>
+  );
+};
 
 export default App;
